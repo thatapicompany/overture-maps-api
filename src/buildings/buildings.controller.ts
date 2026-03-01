2d39769f91bdb9dda59fed62a856f847922a9fb9
aden forshaw <aden.forshaw@gmail.com>
2026-03-01 15:58:38 +1100

    feat: Enhance API security by integrating Helmet, adding an authentication guard to building endpoints, and implementing parameterized BigQuery queries.

--- src/buildings/buildings.controller.ts
+++ src/buildings/buildings.controller.ts
@@ -2,39 +2,41 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Controller, Logger, Query,Get } from '@nestjs/common';
+import { Controller, Logger, Query, Get } from '@nestjs/common';
 import { BuildingsService } from './buildings.service';
 import { GetBuildingsQuery } from './dto/requests/get-buildings-query.dto';
 import { BuildingDto, toBuildingDto } from './dto/responses/building-response.dto';
 import { wrapAsGeoJSON } from '../utils/geojson';
 import { Format } from '../common/dto/requests/get-by-location.dto';
 import { ApiSecurity, ApiTags } from '@nestjs/swagger';
-
+import { UseGuards } from '@nestjs/common';
+import { IsAuthenticatedGuard } from '../guards/is-authenticated.guard';
 
 @ApiTags('Buildings')
 @ApiSecurity('API_KEY') // Applies the API key security scheme defined in Swagger
 @Controller('buildings')
-export class BuildingsController { 
+@UseGuards(IsAuthenticatedGuard)
+export class BuildingsController {
 
-    logger = new Logger('BuildingsController');
+  logger = new Logger('BuildingsController');
 
-    constructor(
-        private readonly buildingsService: BuildingsService,
-    ) {}
+  constructor(
+    private readonly buildingsService: BuildingsService,
+  ) { }
 
-    @Get()
-    async getBuildings(@Query() query: GetBuildingsQuery): Promise<BuildingDto[]|any> {
+  @Get()
+  async getBuildings(@Query() query: GetBuildingsQuery): Promise<BuildingDto[] | any> {
 
-        
-        const buildings = await this.buildingsService.getBuildings(query);
 
-        const dtoResults = buildings.map((building: any) => toBuildingDto(building, query));
-        
-        if(query.format ===  Format.GEOJSON) {
-            return wrapAsGeoJSON(dtoResults)  
-          }else{
-            return dtoResults
-          }
+    const buildings = await this.buildingsService.getBuildings(query);
+
+    const dtoResults = buildings.map((building: any) => toBuildingDto(building, query));
+
+    if (query.format === Format.GEOJSON) {
+      return wrapAsGeoJSON(dtoResults)
+    } else {
+      return dtoResults
     }
+  }
 
 }
